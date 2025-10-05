74ca2fb3e27b733b6fb447fd023506a374c30624
isusha <aisuabdikarimova@gmail.com>
2025-10-05 19:58:07 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,25 +2,26 @@ import logging
 import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-from forecast import get_air_quality
+from forecast import aqi_message
 
-# токен телеграм-бота
+# Токен бота в переменной окружения BOT_TOKEN
 API_TOKEN = os.getenv("BOT_TOKEN")
+
+if not API_TOKEN:
+    raise ValueError("Не задан BOT_TOKEN!")
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
     await message.answer(
         "Привет! Я бот качества воздуха 🌍\n"
         "Напиши /aqi <город> и я покажу актуальные данные.\n"
-        "Например: /aqi London"
+        "Пример: /aqi Almaty"
     )
-
 
 @dp.message_handler(commands=["aqi"])
 async def aqi_handler(message: types.Message):
@@ -30,10 +31,8 @@ async def aqi_handler(message: types.Message):
         return
 
     city = parts[1]
-    result = get_air_quality(city)
-
+    result = aqi_message(city)
     await message.reply(result)
-
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
